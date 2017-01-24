4d162fd379775bc27f536a34c998921d30989c07
matthewbay <baykenney@gmail.com>
2017-01-24 16:33:09 -0500

    simplify

--- app/models/project.rb
+++ app/models/project.rb
@@ -18,7 +18,7 @@ class Project < ActiveRecord::Base
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
     def shortname
-    	name.length > 25? name[0..25] + "..." : name
+    	name.length > 40? name[0..40] + "..." : name
     end
 
     def average_rating
